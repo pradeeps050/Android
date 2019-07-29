2e0a5ed57b0a6276765f572a2405e540ea07272f
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-07-29 19:12:21 +0530

    model and repository class for home screen.

---
+++ app/src/main/java/com/shopping/feature/home/data/model/CategoryOffer.java
@@ -0,0 +1,33 @@
+package com.shopping.feature.home.data.model;
+
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
+
+import com.shopping.R;
+
+import java.util.ArrayList;
+
+public class CategoryOffer {
+
+    public String imageUrl;
+
+    public CategoryOffer(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @BindingAdapter("android:imageUrl")
+    public static void loadImage(ImageView imageView, String imageUrl) {
+        imageView.setImageResource(R.drawable.banner_3);
+    }
+
+    public ArrayList<CategoryOffer> getData() {
+        ArrayList<CategoryOffer> list = new ArrayList<>();
+        list.add(new CategoryOffer(""));
+        list.add(new CategoryOffer(""));
+        list.add(new CategoryOffer(""));
+        list.add(new CategoryOffer(""));
+        list.add(new CategoryOffer(""));
+        return list;
+    }
+
+}
---
+++ app/src/main/java/com/shopping/feature/home/data/model/Product.java
@@ -0,0 +1,45 @@
+package com.shopping.feature.home.data.model;
+
+import android.databinding.BindingAdapter;
+import android.media.Image;
+import android.widget.ImageView;
+
+import com.shopping.R;
+
+import java.util.ArrayList;
+
+public class Product {
+    public String detail;
+    public String price;
+    public String discount;
+    public String quantity;
+    public static String imageUrl;
+
+    public Product(String detail, String price, String discount, String quantity) {
+        this.detail = detail;
+        this.price = price;
+        this.discount = discount;
+        this.quantity = quantity;
+    }
+
+    @BindingAdapter("android:imageUrl")
+    public static void loadProductImage(ImageView imageView, String imageUrl) {
+        imageView.setImageResource(R.drawable.banner_2);
+    }
+
+    public static ArrayList<Product> getProduct() {
+        ArrayList<Product> products = new ArrayList<>();
+        products.add(new Product("Mothers choice Kacchi \n Ghanni Mustered )il",
+                "78", "171", "1kg"));
+        products.add(new Product("Mothers choice Kacchi \n Ghanni Mustered )il",
+                "78", "171", "1kg"));
+        products.add(new Product("Mothers choice Kacchi \n Ghanni Mustered )il",
+                "78", "171", "1kg"));
+        products.add(new Product("Mothers choice Kacchi \n Ghanni Mustered )il",
+                "78", "171", "1kg"));
+        products.add(new Product("Mothers choice Kacchi \n Ghanni Mustered )il",
+                "78", "171", "1kg"));
+
+        return products;
+    }
+ }
