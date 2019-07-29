39850a22f99a870bf46272136d897adbc7a0e375
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-07-29 19:13:56 +0530

    Added splash screen.

---
+++ app/src/main/java/com/shopping/feature/product/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.shopping.feature.product;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.shopping.R;
+import com.shopping.feature.changepassword.ChangePasswordActivity;
+import com.shopping.feature.login.ui.login.LoginActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                finish();
+                }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
