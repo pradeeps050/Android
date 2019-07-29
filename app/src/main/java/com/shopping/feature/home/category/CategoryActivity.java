988d9e82a31ebda3f3668ab78f37c05fcd36e066
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-07-29 19:12:48 +0530

    Added category activity.

---
+++ app/src/main/java/com/shopping/feature/home/category/CategoryActivity.java
@@ -0,0 +1,16 @@
+package com.shopping.feature.home.category;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shopping.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
+
