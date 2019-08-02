4a1500aadae8bf185a898e2e1af208352c0adc3e
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-02 11:48:55 +0530

    Implemented code for testing api login api.

--- app/src/main/java/com/shopping/feature/login/data/LoginRepository.java
+++ app/src/main/java/com/shopping/feature/login/data/LoginRepository.java
@@ -1,12 +1,39 @@
 package com.shopping.feature.login.data;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.shopping.feature.login.data.model.LoggedInUser;
+import com.shopping.feature.login.data.model.LoginResponse;
+import com.shopping.feature.login.data.model.User;
+import com.shopping.feature.login.ui.login.LoginActivity;
+import com.shopping.feature.registration.SignUpResult;
+import com.shopping.framework.application.AppInstance;
+import com.shopping.framework.network.RestApi;
+import com.shopping.framework.network.RestApiBuilder;
+import com.shopping.framework.network.RestApiClient;
+import com.shopping.framework.network.RestApiServices;
+import com.shopping.framework.preference.PreferenceHelper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import timber.log.Timber;
 
 /**
  * Class that requests authentication and user information from the remote data source and
  * maintains an in-memory cache of login status and user credentials information.
  */
 public class LoginRepository {
+    private static final String TAG = LoginRepository.class.getSimpleName();
 
     private static volatile LoginRepository instance;
 
@@ -15,6 +42,7 @@ public class LoginRepository {
     // If user credentials will be cached in local storage, it is recommended it be encrypted
     // @see https://developer.android.com/training/articles/keystore
     private LoggedInUser user = null;
+    Result<LoggedInUser> result = null;
 
     // private constructor : singleton access
     private LoginRepository(LoginDataSource dataSource) {
@@ -45,10 +73,104 @@ public class LoginRepository {
 
     public Result<LoggedInUser> login(String username, String password) {
         // handle login
-        Result<LoggedInUser> result = dataSource.login(username, password);
-        if (result instanceof Result.Success) {
-            setLoggedInUser(((Result.Success<LoggedInUser>) result).getData());
-        }
+        Log.d(TAG, ">>> login  ");
+
+        RestApiBuilder.getNetworkService(RestApi.class).login("password", "ps@gmail.com",
+                "Viraj@2019")
+                .enqueue(new Callback<LoginResponse>() {
+                    @Override
+                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                        if (response.isSuccessful()) {
+                            Log.d(TAG, ">>> Response CODE>>> " + response.code());
+                            LoginResponse loginResponse = response.body();
+                            if(loginResponse != null) {
+                                Log.d(TAG, "Login Response >> Access Token >> " + loginResponse.accessToken);
+                                Log.d(TAG, "Login Response >> Token type >> " + loginResponse.tokenType);
+                                Log.d(TAG, "Login Response >> Expire In>>  " + loginResponse.expiresIn);
+                                Log.d(TAG, "Login Response >> Refresh Token>>  " + loginResponse.refreshToken);
+                                saveLoginResponse(loginResponse);
+                            } else {
+                                Log.d(TAG, ">> Login Response is NULL");
+                            }
+                        } else {
+                            Log.d(TAG, ">> Response is UNSuccessful");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResponse> call, Throwable t) {
+                        Log.d(TAG, "On Failure");
+
+                    }
+                });
         return result;
     }
+
+    public void getUser(String token) {
+        Log.d(TAG, ">> getUser>> ");
+        RestApiBuilder.getNetworkService(RestApi.class)
+                .getUserByToken("Bearer " + token)
+                .enqueue(new Callback<User>() {
+
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        if (response.isSuccessful()) {
+                            Log.d(TAG, ">>> Response CODE>>> " + response.code());
+                            User user = response.body();
+                            if (user != null) {
+                                Log.i(TAG, ">> User Details >>> " + user.toString());
+
+                            } else {
+                                Log.w(TAG, "User is NULL ");
+                            }
+
+                        } else {
+
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+
+                    }
+                });
+
+
+    }
+
+    public void saveLoginResponse(LoginResponse loginResponse) {
+       Context context =  AppInstance.getInstance().getContext();
+       if (context != null) {
+           PreferenceHelper helper = PreferenceHelper.getAppPrefs(AppInstance.getInstance().getContext());
+           helper.saveSaveLoginResponse(loginResponse);
+           getUser(loginResponse.accessToken);
+       } else {
+           Log.w(TAG, ">> App context is NULL");
+       }
+    }
+
+    /*public Result<LoggedInUser> login(User user) {
+        Log.d(TAG, ">>> loginUser");
+
+        RestApiBuilder.getNetworkService(RestApi.class).loginUser(new User("8090404700", "Shani@1234", "password"))
+                .enqueue(new Callback<Result<LoggedInUser>>() {
+                    @Override
+                    public void onResponse(Call<Result<LoggedInUser>> call, Response<Result<LoggedInUser>> response) {
+                        Log.d(TAG, ">>>> Response code" + String.valueOf(response.code()));
+                        Log.d(TAG, ">>>> Response message " + response.message());
+                        Log.d(TAG, ">>> Response " + response.isSuccessful());
+                        result = response.body();
+                        if (result instanceof Result.Success) {
+                            setLoggedInUser(((Result.Success<LoggedInUser>) result).getData());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Result<LoggedInUser>> call, Throwable t) {
+                        Log.d(TAG, "onFailure ");
+
+                    }
+                });
+        return result;
+
+    }*/
 }
