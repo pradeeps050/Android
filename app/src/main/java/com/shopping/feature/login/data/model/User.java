083e0c4202253804f52fc15a093284d0d4b97570
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-02 11:47:22 +0530

    added pojo class.

---
+++ app/src/main/java/com/shopping/feature/login/data/model/User.java
@@ -0,0 +1,107 @@
+package com.shopping.feature.login.data.model;
+
+import android.support.annotation.StringRes;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+public class User {
+
+    @SerializedName("UserId")
+    private Integer userId;
+
+    @SerializedName("Email")
+
+    private String email;
+    @SerializedName("Mobile")
+
+    private String mobile;
+    @SerializedName("RoleId")
+    private Integer roleId;
+    @SerializedName("IsVerified")
+
+    private Boolean isVerified;
+    @SerializedName("UserDetails")
+
+    private List<UserDetail> userDetails = new ArrayList<>();
+    @SerializedName("IsActive")
+
+    private Boolean isActive;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Boolean getIsVerified() {
+        return isVerified;
+    }
+
+    public void setIsVerified(Boolean isVerified) {
+        this.isVerified = isVerified;
+    }
+
+    public List<UserDetail> getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(List<UserDetail> userDetails) {
+        this.userDetails = userDetails;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", roleId=" + roleId +
+                ", isVerified=" + isVerified +
+                ", userDetails=" + userDetails.get(0).toString() +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
+
+
+
