e0b794f74c365f218492d549fc1b670aa3bac9b1
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-05 19:27:01 +0530

    Changed login method.

--- app/src/main/java/com/shopping/feature/login/data/LoginRepository.java
+++ app/src/main/java/com/shopping/feature/login/data/LoginRepository.java
@@ -1,5 +1,6 @@
 package com.shopping.feature.login.data;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.content.Context;
 import android.os.Looper;
 import android.util.Log;
@@ -9,13 +10,21 @@ import com.shopping.feature.login.data.model.LoggedInUser;
 import com.shopping.feature.login.data.model.LoginResponse;
 import com.shopping.feature.login.data.model.User;
 import com.shopping.feature.login.ui.login.LoginActivity;
+import com.shopping.feature.login.ui.login.LoginResult;
 import com.shopping.feature.registration.SignUpResult;
+import com.shopping.feature.registration.model.ResponseFail;
+import com.shopping.framework.Executors.AppExecutors;
+import com.shopping.framework.Room.Repository.UserRepository;
 import com.shopping.framework.application.AppInstance;
+import com.shopping.framework.model.UserEntity;
 import com.shopping.framework.network.RestApi;
 import com.shopping.framework.network.RestApiBuilder;
 import com.shopping.framework.network.RestApiClient;
 import com.shopping.framework.network.RestApiServices;
 import com.shopping.framework.preference.PreferenceHelper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -45,7 +54,8 @@ public class LoginRepository {
     // If user credentials will be cached in local storage, it is recommended it be encrypted
     // @see https://developer.android.com/training/articles/keystore
     private LoggedInUser user = null;
-    Result<LoggedInUser> result = null;
+    //Result<LoggedInUser> result = null;
+    Result<User> result;
 
     // private constructor : singleton access
     private LoginRepository(LoginDataSource dataSource) {
@@ -78,61 +88,24 @@ public class LoginRepository {
         this.us = user;
     }
 
-    public Result<User> login(String username, String password) {
+    /*public Result<User> login(String username, String password, MutableLiveData<LoginResult> loginResult ) {
         // handle login
-        Result<User> result = dataSource.login(username, password);
+        result = dataSource.login(username, password, loginResult);
+        if (result instanceof Result.Success) {
+            setLoggedInUser(((Result.Success<User>) result).getData());
+
+        } else {
+            Log.d(TAG, " NOT iNSTANCE of RESULT.SUCCESS");
+        }
+
+        *//*Result<User> result = dataSource.login(username, password);
         if (result instanceof Result.Success) {
             setLoggedInUser(((Result.Success<User>) result).getData());
         } else {
             Log.d(TAG, " NOT iNSTANCE of RESULT.SUCCESS");
 
-        }
+        }*//*
         return result;
-    }
-
-    /*public User login(String username, String password) {
-        // handle login
-        Log.d(TAG, ">>> login  ");
-        if (Looper.myLooper() == Looper.getMainLooper()) {
-            Log.i(TAG, ">>> On MAIN THREAD");
-        }else {
-            Log.i(TAG, ">>> NOT In Main Thred");
-        }
-
-        RestApiBuilder.getNetworkService(RestApi.class).login("password", "ps@gmail.com",
-                "Viraj@2019")
-                .enqueue(new Callback<LoginResponse>() {
-                    @Override
-                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                        if (Looper.myLooper() == Looper.getMainLooper()) {
-                            Log.i(TAG, ">>> On MAIN THREAD");
-                        }else {
-                            Log.i(TAG, ">>> NOT In Main Thred");
-                        }
-                        if (response.isSuccessful()) {
-                            Log.d(TAG, ">>> Response CODE >>> " + response.code());
-                            LoginResponse loginResponse = response.body();
-                            if(loginResponse != null) {
-                                Log.d(TAG, "Login Response >> Access Token >> " + loginResponse.accessToken);
-                                Log.d(TAG, "Login Response >> Token type >> " + loginResponse.tokenType);
-                                Log.d(TAG, "Login Response >> Expire In>>  " + loginResponse.expiresIn);
-                                Log.d(TAG, "Login Response >> Refresh Token>>  " + loginResponse.refreshToken);
-                                loginUser = saveLoginResponse(loginResponse);
-
-                            } else {
-                                Log.w(TAG, ">> Login Response is NULL");
-                            }
-                        } else {
-                            Log.w(TAG, ">> Response is UNSuccessful");
-                        }
-                    }
-                    @Override
-                    public void onFailure(Call<LoginResponse> call, Throwable t) {
-                        Log.w(TAG, "On Failure");
-
-                    }
-                });
-        return loginUser;
     }*/
 
     public User getUser(String token) {
@@ -153,7 +126,6 @@ public class LoginRepository {
                             } else {
                                 Log.w(TAG, "User is NULL ");
                             }
-
                         } else {
 
                         }
@@ -172,13 +144,88 @@ public class LoginRepository {
        if (context != null) {
            PreferenceHelper helper = PreferenceHelper.getAppPrefs(AppInstance.getInstance().getContext());
            helper.saveSaveLoginResponse(loginResponse);
-           user = getUser(loginResponse.accessToken);
        } else {
            Log.w(TAG, ">> App context is NULL");
        }
        return user;
     }
 
+    public void login(String email, String password,
+                      MutableLiveData<User> mutableLiveData, MutableLiveData<ResponseFail> failMutableLiveData) {
+        RestApiBuilder.getNetworkService(RestApi.class).login("password", email, password).
+                enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if (response.isSuccessful()) {
+                    Log.d(TAG, ">>> Response CODE >>> " + response.code());
+                    LoginResponse loginResponse = response.body();
+                    if(loginResponse != null) {
+                        Log.d(TAG, "Login Response >> Access Token >> " + loginResponse.accessToken);
+                        Log.d(TAG, "Login Response >> Token type >> " + loginResponse.tokenType);
+                        Log.d(TAG, "Login Response >> Expire In>>  " + loginResponse.expiresIn);
+                        Log.d(TAG, "Login Response >> Refresh Token>>  " + loginResponse.refreshToken);
+                        saveLoginResponse(loginResponse);
+                        Log.d(TAG, ">> Passing token >> ");
+
+                        RestApiBuilder.getNetworkService(RestApi.class)
+                                .getUserByToken("Bearer " + loginResponse.accessToken)
+                                .enqueue(new Callback<User>() {
+
+                                    @Override
+                                    public void onResponse(Call<User> call, Response<User> response) {
+                                        if (response.isSuccessful()) {
+                                            Log.d(TAG, ">>> Response CODE>>> " + response.code());
+                                            User user = response.body();
+                                            if (user != null) {
+                                                Log.i(TAG, ">> User Details >>> " + user.toString());
+                                                mutableLiveData.postValue(user);
+                                                saveUserInDatabase(user);
+                                            } else {
+                                                Log.w(TAG, ">> User is NULL >>");
+                                            }
+                                        } else {
+                                            Log.e(TAG, ">> Not access user profile >>");
+                                        }
+                                    }
+                                    @Override
+                                    public void onFailure(Call<User> call, Throwable t) {
+                                        Log.e(TAG, ">> User profile onFail >> ");
+                                    }
+                                });
+
+                    } else {
+                        Log.w(TAG, ">> Login Response is NULL");
+                    }
+                } else {
+                    Log.w(TAG, ">> Response is FALSE while login >> ");
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.errorBody().string());
+                        String message = jsonObject.getString("Message");
+                        Log.w(TAG, ">> + " +  message);
+                        failMutableLiveData.postValue(new ResponseFail(message));
+                    } catch (IOException e) {
+                        Log.e(TAG, ">> " + e.getMessage());
+                    } catch (JSONException e) {
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                Log.e(TAG, ">> Login Fail >>");
+            }
+        });
+    }
+
+    public void saveUserInDatabase(User user) {
+        Log.i(TAG, ">> saving in database >> ");
+        UserRepository userRepository = new UserRepository(AppInstance.getInstance().getContext());
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail(user.getEmail());
+        userEntity.setFullName(user.getUserDetails()!= null && user.getUserDetails().size()!= 0 ? user.getUserDetails().get(0).getFullName() : null);
+        userEntity.setMobile(user.getMobile().intValue());
+        userRepository.insert(userEntity);
+    }
 
     /*public Result<LoggedInUser> login(User user) {
         Log.d(TAG, ">>> loginUser");
