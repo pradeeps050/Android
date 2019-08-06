9339436c4fa8b872c01a46f1bc999e1750fa82d6
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-06 17:07:44 +0530

    Implemented method for network call

--- app/src/main/java/com/shopping/feature/registration/viewmodel/OTPViewModel.java
+++ app/src/main/java/com/shopping/feature/registration/viewmodel/OTPViewModel.java
@@ -1,6 +1,37 @@
 package com.shopping.feature.registration.viewmodel;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.util.Log;
+
+import com.shopping.feature.registration.data.OTPRepository;
+
+import okhttp3.ResponseBody;
 
 public class OTPViewModel extends ViewModel {
+    private static final String TAG = OTPViewModel.class.getSimpleName();
+    private MutableLiveData<ResponseBody> mutableLiveData = new MutableLiveData<>();
+
+    private OTPRepository repository;
+
+    public OTPViewModel() {
+        Log.d(TAG, ">> OTP View Model >> ");
+        repository = OTPRepository.getInstance();
+    }
+
+    public void requestOtp(int userId) {
+        repository.requestOtp(userId, mutableLiveData);
+    }
+
+    public void resendOtp(int userId) {
+        repository.resendOtp(userId);
+    }
+
+    public void verifyOtp(int userId, int otp) {
+        repository.verifyOtp(userId, otp);
+    }
+
+
+
+
 }
