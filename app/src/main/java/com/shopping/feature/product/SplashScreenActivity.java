f55a3cad937c0f493490a58cf80bf51a3255682b
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-06 19:32:18 +0530

    Query database user for details.

--- app/src/main/java/com/shopping/feature/product/SplashScreenActivity.java
+++ app/src/main/java/com/shopping/feature/product/SplashScreenActivity.java
@@ -1,16 +1,21 @@
 package com.shopping.feature.product;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
 import com.shopping.R;
-import com.shopping.feature.changepassword.ChangePasswordActivity;
+import com.shopping.feature.home.HomeActivity;
 import com.shopping.feature.login.ui.login.LoginActivity;
+import com.shopping.framework.Room.Database.ShoppingRoomDatabase;
+import com.shopping.framework.application.AppInstance;
+import com.shopping.framework.model.UserEntity;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private static final String TAG = SplashScreenActivity.class.getSimpleName();
 
     private static int SPLASH_SCREEN_TIME_OUT = 2000;
 
@@ -27,5 +32,26 @@ public class SplashScreenActivity extends AppCompatActivity {
                 finish();
                 }
         }, SPLASH_SCREEN_TIME_OUT);
+
+        /*AppInstance.getInstance().getContext().getExecutors().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, ">> Querying Db for user ");
+                ShoppingRoomDatabase db = ShoppingRoomDatabase.getInstance(SplashScreenActivity.this);
+                if (db != null) {
+                    UserEntity entity = db.userDao().getUserDetail();
+                    if (entity!= null) {
+                        if ((entity.getIsVerified() == 1) ? true : false) {
+                            startActivity(new Intent(SplashScreenActivity.this, HomeActivity.class));
+                        } else {
+                            startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                        }
+                        Log.d("Users ", ">> " + entity.toString());
+                    }
+                }
+
+            }
+        });*/
+
     }
 }
