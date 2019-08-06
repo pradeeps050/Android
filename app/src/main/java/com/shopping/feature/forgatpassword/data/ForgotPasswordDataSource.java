5fc3393cdd66adf546065af3942cc55d2caa3fdf
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-06 19:25:17 +0530

    Added class

---
+++ app/src/main/java/com/shopping/feature/forgatpassword/data/ForgotPasswordDataSource.java
@@ -0,0 +1,47 @@
+package com.shopping.feature.forgatpassword.data;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
+
+import com.shopping.framework.network.RestApi;
+import com.shopping.framework.network.RestApiBuilder;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ForgotPasswordDataSource {
+    private static final String TAG = ForgotPasswordDataSource.class.getSimpleName();
+    private static  ForgotPasswordDataSource instance;
+
+    public static ForgotPasswordDataSource getInstance() {
+        if (instance == null) {
+            instance = new ForgotPasswordDataSource();
+        }
+        return instance;
+    }
+
+    public void sendOtp(String mobile, MutableLiveData<ForgotPassword> mutableLiveData) {
+        RestApiBuilder.getNetworkService(RestApi.class).sendOtp(mobile)
+                .enqueue(new Callback<ForgotPassword>() {
+                    @Override
+                    public void onResponse(Call<ForgotPassword> call, Response<ForgotPassword> response) {
+                        if (response.isSuccessful() && response.code() == 200) {
+                            mutableLiveData.postValue(response.body());
+                        } else {
+                            Log.w(TAG, " >> false");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ForgotPassword> call, Throwable t) {
+                        Log.w(TAG, " >> onFailure");
+
+                    }
+                });
+    }
+
+}
