26d48afc5e5fad118c3075188c112f5ebcae062c
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-06 19:28:08 +0530

    Added method verify otp.

--- app/src/main/java/com/shopping/feature/registration/viewmodel/OTPViewModel.java
+++ app/src/main/java/com/shopping/feature/registration/viewmodel/OTPViewModel.java
@@ -1,12 +1,18 @@
 package com.shopping.feature.registration.viewmodel;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.util.Log;
 
 import com.shopping.feature.registration.data.OTPRepository;
+import com.shopping.framework.network.RestApi;
+import com.shopping.framework.network.RestApiBuilder;
 
 import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class OTPViewModel extends ViewModel {
     private static final String TAG = OTPViewModel.class.getSimpleName();
@@ -19,6 +25,10 @@ public class OTPViewModel extends ViewModel {
         repository = OTPRepository.getInstance();
     }
 
+    public LiveData<ResponseBody> getMutable() {
+        return mutableLiveData;
+    }
+
     public void requestOtp(int userId) {
         repository.requestOtp(userId, mutableLiveData);
     }
@@ -29,6 +39,10 @@ public class OTPViewModel extends ViewModel {
 
     public void verifyOtp(int userId, int otp) {
         repository.verifyOtp(userId, otp);
+    }
+
+    public void validateOtp(int userId, String otp) {
+        repository.validateOtp(userId, otp, mutableLiveData);
     }
 
 
