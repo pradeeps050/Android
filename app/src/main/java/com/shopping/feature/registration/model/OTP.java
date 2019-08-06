a07b6efab4842da85d4566fdc70f49649dd442e3
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-06 19:33:10 +0530

    implemented json parsing of OTP.

--- app/src/main/java/com/shopping/feature/registration/model/OTP.java
+++ app/src/main/java/com/shopping/feature/registration/model/OTP.java
@@ -1,13 +1,33 @@
 package com.shopping.feature.registration.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class OTP {
-    private int otp;
+    @SerializedName("UserId")
+    private Integer userId;
+    @SerializedName("Email")
+    private String email;
+    @SerializedName("Mobile")
+    private String mobile;
+    @SerializedName("Password")
+    private String password;
+    @SerializedName("RoleId")
+    private Integer roleId;
+    @SerializedName("IsVerified")
+    private Boolean isVerified;
+    @SerializedName("IsActive")
+    private Boolean isActive;
 
-    public OTP(int otp) {
-        this.otp = otp;
-    }
-
-    public int getOtp() {
-        return otp;
+    @Override
+    public String toString() {
+        return "OTP{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", password='" + password + '\'' +
+                ", roleId=" + roleId +
+                ", isVerified=" + isVerified +
+                ", isActive=" + isActive +
+                '}';
     }
 }
