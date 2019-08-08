4432024858ca863ff6f1986b77bc67c2edaf1dd3
Akankshasoni <akankshasoni693@gmail.com>
2019-08-08 17:28:29 +0530

    remove scrollline in need help screen .

--- app/src/main/java/com/shopping/feature/needhelp/ui/NeedHelpActivity.java
+++ app/src/main/java/com/shopping/feature/needhelp/ui/NeedHelpActivity.java
@@ -7,13 +7,15 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.EventLog;
+import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebView;
 
 import com.shopping.R;
 import com.shopping.databinding.ActivityNeedHelpBinding;
-import com.shopping.feature.needhelp.data.model.NeedHelpModel;
-import com.shopping.feature.thankyou.ui.ThankYouActivity;
+import com.shopping.feature.addaddress.ui.AddAddressActivity;
+
 
 public class NeedHelpActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -29,36 +31,41 @@ public class NeedHelpActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Need Help/ FAQ");
         toolbar.setNavigationIcon(R.drawable.ic_icon_back);
 
-        binding.web1.loadData(getResources().getString(R.string.message2),"text/html; charset=utf-8","utf-8");
-        binding.web2.loadData(getResources().getString(R.string.message3),"text/html; charset=utf-8","utf-8");
-        binding.web3.loadData(getResources().getString(R.string.message2),"text/html; charset=utf-8","utf-8");
-        binding.web4.loadData(getResources().getString(R.string.message3),"text/html; charset=utf-8","utf-8");
-        binding.web5.loadData(getResources().getString(R.string.message2),"text/html; charset=utf-8","utf-8");
-        binding.web6.loadData(getResources().getString(R.string.message3),"text/html; charset=utf-8","utf-8");
-        binding.web7.loadData(getResources().getString(R.string.message2),"text/html; charset=utf-8","utf-8");
-        binding.web8.loadData(getResources().getString(R.string.message3),"text/html; charset=utf-8","utf-8");
-        binding.web9.loadData(getResources().getString(R.string.message2),"text/html; charset=utf-8","utf-8");
-        binding.web10.loadData(getResources().getString(R.string.message3),"text/html; charset=utf-8","utf-8");
-        binding.web11.loadData(getResources().getString(R.string.message2),"text/html; charset=utf-8","utf-8");
-        binding.web12.loadData(getResources().getString(R.string.message3),"text/html; charset=utf-8","utf-8");
-        binding.view1.setOnClickListener(new View.OnClickListener() {
+        binding.web1.loadData(getResources().getString(R.string.message2), "text/html; charset=utf-8", "utf-8");
+        binding.web2.loadData(getResources().getString(R.string.message3), "text/html; charset=utf-8", "utf-8");
+        binding.web3.loadData(getResources().getString(R.string.message2), "text/html; charset=utf-8", "utf-8");
+        binding.web4.loadData(getResources().getString(R.string.message3), "text/html; charset=utf-8", "utf-8");
+        binding.web5.loadData(getResources().getString(R.string.message2), "text/html; charset=utf-8", "utf-8");
+        binding.web6.loadData(getResources().getString(R.string.message3), "text/html; charset=utf-8", "utf-8");
+        binding.web7.loadData(getResources().getString(R.string.message2), "text/html; charset=utf-8", "utf-8");
+        binding.web8.loadData(getResources().getString(R.string.message3), "text/html; charset=utf-8", "utf-8");
+        binding.web9.loadData(getResources().getString(R.string.message2), "text/html; charset=utf-8", "utf-8");
+        binding.web10.loadData(getResources().getString(R.string.message3), "text/html; charset=utf-8", "utf-8");
+        binding.web11.loadData(getResources().getString(R.string.message2), "text/html; charset=utf-8", "utf-8");
+        binding.web12.loadData(getResources().getString(R.string.message3), "text/html; charset=utf-8", "utf-8");
+
+
+        changeColor(R.color.black);
+        binding.btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(NeedHelpActivity.this, ThankYouActivity.class);
+                Intent i =new Intent(NeedHelpActivity.this, AddAddressActivity.class);
                 startActivity(i);
             }
         });
-        changeColor(R.color.black);
-    }
-    public void changeColor(int resourseColor)
-    {
-     if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP)
-     {
-         getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(),resourseColor));
 
-     }
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return (motionEvent.getAction()== MotionEvent.ACTION_MOVE);
+            }
+        });
     }
 
+    public void changeColor(int resourseColor) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(), resourseColor));
 
-
+        }
+    }
 }
