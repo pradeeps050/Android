5291aa96f1a8d6982319b8c3f99219a74a817c4d
Akankshasoni <akankshasoni693@gmail.com>
2019-08-08 18:44:44 +0530

    DateTimePaymentactivity has been included

--- app/src/main/java/com/shopping/feature/datetimepayment/ui/DateTimePaymentActivity.java
+++ app/src/main/java/com/shopping/feature/datetimepayment/ui/DateTimePaymentActivity.java
@@ -1,14 +1,17 @@
 package com.shopping.feature.datetimepayment.ui;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.shopping.R;
 import com.shopping.databinding.ActivityDateTimePaymentBinding;
+import com.shopping.feature.deliveredandrating.ui.DeliveredAndRatingActivity;
 
 public class DateTimePaymentActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -26,6 +29,15 @@ public class DateTimePaymentActivity extends AppCompatActivity {
         //
         String couponCode= binding.enterCouponEdt.getText().toString();
         changeColor(R.color.black);
+        binding.applyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DateTimePaymentActivity.this, DeliveredAndRatingActivity.class);
+                startActivity(i);
+
+            }
+        });
+        changeColor(R.color.shadow_light_black_background);
     }
 
     public void changeColor(int resourseColor) {
