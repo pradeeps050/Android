f496e3a88acf6fbad631b5cb1194fb0d4cb9e6f7
Akankshasoni <akankshasoni693@gmail.com>
2019-08-08 18:47:06 +0530

    update

--- app/src/main/java/com/shopping/feature/thankyou/ui/ThankYouActivity.java
+++ app/src/main/java/com/shopping/feature/thankyou/ui/ThankYouActivity.java
@@ -3,23 +3,33 @@ package com.shopping.feature.thankyou.ui;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.shopping.R;
-import com.shopping.databinding.ActivityNeedHelpBinding;
 import com.shopping.databinding.ActivityThankYouBinding;
 import com.shopping.feature.addaddress.ui.AddAddressActivity;
 
 public class ThankYouActivity extends AppCompatActivity {
-
+private Toolbar toolbar;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_thank_you);
-        ActivityThankYouBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_thank_you);
+         setContentView(R.layout.activity_thank_you);
+       ActivityThankYouBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_thank_you);
+        toolbar = (Toolbar) binding.whiteToolbar;
+
+       setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        toolbar.setNavigationIcon(R.drawable.ic_icon_back_b);
+
         binding.shareImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,6 +41,7 @@ public class ThankYouActivity extends AppCompatActivity {
                 startActivity(Intent.createChooser(shareIntent, "Share via"));
             }
         });
+
         binding.orderStatus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,12 +49,14 @@ public class ThankYouActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        changeColor(R.color.black);
-    }
 
-    public void changeColor(int resourseColor) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(), resourseColor));
+
+// for changing the color of status bar
+           changeColor(R.color.white);
+    }
+         public void changeColor(int resourseColor) {
+              if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+              getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(), resourseColor));
         }
     }
 }
