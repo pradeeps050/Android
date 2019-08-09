77965312df50f915af9ab4e0f3311f37307cd140
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-09 18:28:19 +0530

    Added constants.

--- app/src/main/java/com/shopping/framework/constantsValues/ConstantValues.java
+++ app/src/main/java/com/shopping/framework/constantsValues/ConstantValues.java
@@ -10,4 +10,11 @@ public interface ConstantValues {
     String LOG_TAG = "Shopping";
     String BUNDLE_DATA = "BUNDLE_DATA";
     String VERTICAL_STRING = "O\nF\nF\nE\nR";
+    String LOGIN_GRANT = "password";
+    String CHANGE_PASSWORD = "ChangePassword";
+    String USER_ID = "UserId";
+    String FORGOT_PASSWOED = "ForgotPassword";
+    String FLAG_FORGOT_PASSWORD = "ForgotPassword";
+    String FLAG_LOGIN = "Login";
+    String FLAG_CHANGE_PASSWORD = "ChangePassword";
 }
