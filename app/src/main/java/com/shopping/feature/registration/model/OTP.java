9a145639dcae842b1fd20b01a01c69ecee47a388
pSharma05 <Pradeep.sharma@orangeskill.com>
2019-08-10 18:23:08 +0530

    added getter setter.

--- app/src/main/java/com/shopping/feature/registration/model/OTP.java
+++ app/src/main/java/com/shopping/feature/registration/model/OTP.java
@@ -18,6 +18,62 @@ public class OTP {
     @SerializedName("IsActive")
     private Boolean isActive;
 
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Boolean getVerified() {
+        return isVerified;
+    }
+
+    public void setVerified(Boolean verified) {
+        isVerified = verified;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
     @Override
     public String toString() {
         return "OTP{" +
